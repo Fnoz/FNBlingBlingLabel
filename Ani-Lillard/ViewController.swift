b04c93b41355a1518df672164ef9c2cf829b3c5d
LuWeijun <luweijun@weidian.com>
2016-05-29 20:49:31 +0800

    Initial Commit

---
+++ Ani-Lillard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ani-Lillard
+//
+//  Created by Fnoz on 16/5/29.
+//  Copyright © 2016年 Fnoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
